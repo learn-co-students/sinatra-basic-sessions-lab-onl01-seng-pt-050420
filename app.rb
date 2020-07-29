eedcbbccc942e48148f1bf5a920dacbcc7ebed2e
C0mbed <matt@farncombe.co>
2020-07-29 12:05:08 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "coding_rules"
+    end
+
+    get '/' do 
+        session.clear
+        erb :index
+        
+    end
+
+    post '/checkout' do
+        @session = session
+        @session["item"] = params[:item]
+     
+        erb :display
+    end
+
 end
