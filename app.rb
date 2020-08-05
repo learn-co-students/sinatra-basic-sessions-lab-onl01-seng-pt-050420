6d8f610ea89613e53550509cb0aaf9a1b7bedf54
ValentinaPanic <valentina.panic87@gmail.com>
2020-08-04 23:52:06 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sesions
+        set :session_secret, "secret"
+    end
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        # "hello"
+        session[:item] = params[:item]
+        @session = session
+       
+        erb :show
+    end
 end
